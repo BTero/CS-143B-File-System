949ad1ce99b10e23820e8fbea9fa95b3d3efc0c5
Brian Tero <Btero@uci.edu>
2014-05-21 22:20:22 -0700

    Added Base files for project.

---
+++ src/Shell.java
@@ -0,0 +1,11 @@
+
+public class Shell {
+	
+	private static String studentName = "Brian Tero";
+	private static String studentID = "90121402";
+	private static String uciNetID = "BTero";
+
+	public void Shell(){
+		
+	}
+}
